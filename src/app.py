823bad264987acd72615b4f03e1da1ae0f76fc10
AjayArvind2207 <alwayssactheexchange@gmail.com>
2022-02-26 11:55:46 +0800

    Added tasks.html and logos

--- src/app.py
+++ src/app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 app.secret_key = 'Secret Key 123'
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET', 'POST'])
 def home():
     '''Displays home page'''
+    if request.method == 'POST':
+        print(list(request.form.items()))
     return render_template("index.html")
 
 @app.route("/form", methods=['GET'])
@@ -19,9 +21,17 @@ def form():
 
 @app.route("/game", methods=['GET'])
 def game():
-    '''Displays form page'''
+    '''Displays game page'''
     return render_template("gamification.html")
 
+@app.route("/support", methods=['GET'])
+def support():
+    '''Displays form page'''
+    return render_template("support.html")
+
+@app.route("/tasks", methods=['GET'])
+def tasks():
+    '''Displays form page'''
+    return render_template("tasks.html", course = "stonk")
 if __name__ == "__main__":
-    app.PORT = 5000
-    app.run(port=app.PORT)
+    app.run(debug = True)
