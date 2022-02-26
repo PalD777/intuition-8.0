ba35906fac180d5b28764961f77a47b1b11ba2f2
PalD777 <droliap1@gmail.com>
2022-02-26 12:16:10 +0800

    [*] Made tasks dynamically load from files

--- src/app.py
+++ src/app.py
@@ -1,11 +1,15 @@
 from pathlib import Path
-from flask import Flask, jsonify, render_template, request, session
+from flask import Flask, jsonify, render_template, request, session, Markup
 
 
 # Sets up the Flask application
 app = Flask(__name__)
 app.secret_key = 'Secret Key 123'
+tasks_data = {}
 
+class user:
+    '''Test class to temp store stuff of db'''
+    pass
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -32,6 +36,16 @@ def support():
 @app.route("/tasks", methods=['GET'])
 def tasks():
     '''Displays form page'''
-    return render_template("tasks.html", course = "stonk")
+    key = sorted(tasks_data.keys())[user.task_no]
+    return render_template("tasks.html", course = key, desc=tasks_data[key])
+
+def get_tasks():
+    for file in sorted((Path(__file__).parent / 'tasks').glob('*.txt')):
+        # Ensure users can't input to files or it will lead to server-side XSS
+        tasks_data[file.stem] = Markup(open(file).read().replace('\n', '<br>'))
+
 if __name__ == "__main__":
+    get_tasks()
+    print(tasks_data)
+    user.task_no = 1
     app.run(debug = True)
