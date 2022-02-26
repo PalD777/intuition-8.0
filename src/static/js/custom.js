09a0bf46ab823ce9242d6f54a45f92b6780b3a3f
PalD777 <droliap1@gmail.com>
2022-02-26 21:39:17 +0800

    [*] Massive bug fix

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -1,38 +1,200 @@
-/*------------------------------------------------------
-    Author : www.webthemez.com
-    License: Commons Attribution 3.0
-    http://creativecommons.org/licenses/by/3.0/
----------------------------------------------------------  */
-
 (function ($) {
-    "use strict";
-    var mainApp = {
+	
+	"use strict";
 
-        initFunction: function () {
-            /*MENU 
-            ------------------------------------*/
+	// Header Type = Fixed
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    var box = $('.header-text').height();
+    var header = $('header').height();
 
-			
-            $(window).bind("load resize", function () {
-                if ($(this).width() < 768) {
-                    $('div.sidebar-collapse').addClass('collapse')
-                } else {
-                    $('div.sidebar-collapse').removeClass('collapse')
-                }
-            });      
-     
-        },
-
-        initialization: function () {
-            mainApp.initFunction();
-
-        }
-
+    if (scroll >= box - header) {
+      $("header").addClass("background-header");
+    } else {
+      $("header").removeClass("background-header");
     }
-    // Initializing ///
+  });
 
-    $(document).ready(function () {
-        mainApp.initFunction();
+
+	$('.loop').owlCarousel({
+      center: true,
+      items:1,
+      loop:true,
+      autoplay: true,
+      nav: true,
+      margin:0,
+      responsive:{ 
+          1200:{
+              items:5
+          },
+          992:{
+              items:3
+          },
+          760:{
+            items:2
+        }
+      }
+  });
+  
+  $("#modal_trigger").leanModal({
+		top: 100,
+		overlay: 0.6,
+		closeButton: ".modal_close"
+});
+
+$(function() {
+		// Calling Login Form
+		$("#login_form").click(function() {
+				$(".social_login").hide();
+				$(".user_login").show();
+				return false;
+		});
+
+		// Calling Register Form
+		$("#register_form").click(function() {
+				$(".social_login").hide();
+				$(".user_register").show();
+				$(".header_title").text('Register');
+				return false;
+		});
+
+		// Going back to Social Forms
+		$(".back_btn").click(function() {
+				$(".user_login").hide();
+				$(".user_register").hide();
+				$(".social_login").show();
+				$(".header_title").text('Login');
+				return false;
+		});
+});
+
+  // Acc
+  $(document).on("click", ".naccs .menu div", function() {
+    var numberIndex = $(this).index();
+
+    if (!$(this).is("active")) {
+        $(".naccs .menu div").removeClass("active");
+        $(".naccs ul li").removeClass("active");
+
+        $(this).addClass("active");
+        $(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+
+        var listItemHeight = $(".naccs ul")
+          .find("li:eq(" + numberIndex + ")")
+          .innerHeight();
+        $(".naccs ul").height(listItemHeight + "px");
+      }
+  });
+	
+
+	// Menu Dropdown Toggle
+  if($('.menu-trigger').length){
+    $(".menu-trigger").on('click', function() { 
+      $(this).toggleClass('active');
+      $('.header-area .nav').slideToggle(200);
+    });
+  }
+
+
+  // Menu elevator animation
+  $('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        var width = $(window).width();
+        if(width < 991) {
+          $('.menu-trigger').removeClass('active');
+          $('.header-area .nav').slideUp(200);  
+        }       
+        $('html,body').animate({
+          scrollTop: (target.offset().top) + 1
+        }, 700);
+        return false;
+      }
+    }
+  });
+
+  $(document).ready(function () {
+      $(document).on("scroll", onScroll);
+      
+      //smoothscroll
+      $('.scroll-to-section a[href^="#"]').on('click', function (e) {
+          e.preventDefault();
+          $(document).off("scroll");
+          
+          $('.scroll-to-section a').each(function () {
+              $(this).removeClass('active');
+          })
+          $(this).addClass('active');
+        
+          var target = this.hash,
+          menu = target;
+          var target = $(this.hash);
+          $('html, body').stop().animate({
+              scrollTop: (target.offset().top) + 1
+          }, 500, 'swing', function () {
+              window.location.hash = target;
+              $(document).on("scroll", onScroll);
+          });
+      });
+  });
+
+  function onScroll(event){
+      var scrollPos = $(document).scrollTop();
+      $('.nav a').each(function () {
+          var currLink = $(this);
+          if (window.location.pathname === currLink[0].pathname) {
+              $('.nav ul li a').removeClass("active");
+              currLink.addClass("active");
+          }
+          else{
+              currLink.removeClass("active");
+          }
+      });
+  }
+
+
+  // Acc
+  $(document).on("click", ".naccs .menu div", function() {
+    var numberIndex = $(this).index();
+
+    if (!$(this).is("active")) {
+        $(".naccs .menu div").removeClass("active");
+        $(".naccs ul li").removeClass("active");
+
+        $(this).addClass("active");
+        $(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+
+        var listItemHeight = $(".naccs ul")
+          .find("li:eq(" + numberIndex + ")")
+          .innerHeight();
+        $(".naccs ul").height(listItemHeight + "px");
+      }
+  });
+
+
+	// Page loading animation
+	 $(window).on('load', function() {
+
+        $('#js-preloader').addClass('loaded');
+
     });
 
-}(jQuery));
+	
+
+	// Window Resize Mobile Menu Fix
+  function mobileNav() {
+    var width = $(window).width();
+    $('.submenu').on('click', function() {
+      if(width < 767) {
+        $('.submenu ul').removeClass('active');
+        $(this).find('ul').toggleClass('active');
+      }
+    });
+  }
+
+
+
+
+})(window.jQuery);
