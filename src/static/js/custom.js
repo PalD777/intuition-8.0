d5c3b779224dedeb00eb97066121450476c5f533
Ayush Garg <themoviechannel66@gmail.com>
2022-02-27 14:36:32 +0800

    fixed css

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -3,18 +3,7 @@
 	"use strict";
 
 	// Header Type = Fixed
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    var box = $('.header-text').height();
-    var header = $('header').height();
-
-    if (scroll >= box - header) {
-      $("header").addClass("background-header");
-    } else {
-      $("header").removeClass("background-header");
-    }
-  });
-
+  
 
 	$('.loop').owlCarousel({
       center: true,
@@ -97,23 +86,7 @@ $(function() {
 
 
   // Menu elevator animation
-  $('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        var width = $(window).width();
-        if(width < 991) {
-          $('.menu-trigger').removeClass('active');
-          $('.header-area .nav').slideUp(200);  
-        }       
-        $('html,body').animate({
-          scrollTop: (target.offset().top) + 1
-        }, 700);
-        return false;
-      }
-    }
-  });
+  
 
   $(document).ready(function () {
       $(document).on("scroll", onScroll);
